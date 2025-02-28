38579a7fe49b6d4e4dbd0ed91969065df6ece094
Maria Lazaridou <lazaridoum.tin24@student.dhbw-heidenheim.de>
2025-02-28 22:45:25 +0100

    Book Club read More Modal done

--- Frontend/Read2Go/src/app/botmjan/botmjan.component.ts
+++ Frontend/Read2Go/src/app/botmjan/botmjan.component.ts
@@ -13,4 +13,11 @@ export class BotmjanComponent {
   bookImage: string = "fourthwing.jpeg"; // Pfad zum Buchbild
   averageRating: number = 4; // Durchschnittliche Sternebewertung
   description: string = `Violet Sorrengail, initially destined for a quiet life in the Scribe Quadrant, is thrust into the perilous world of dragon riders by her formidable mother, where her survival is threatened by her physical fragility, rival cadets, and the ruthless wingleader Xaden Riorson, all while a deadly war looms and a sinister secret within the leadership threatens to unravel everything at Basgiath War College, where the only exits are graduation or death.`;
+  isModalOpen: boolean = false; 
+  openModal() {
+    this.isModalOpen = true; // Modal öffnen
+  }
+  closeModal() {
+    this.isModalOpen = false; // Modal schließen
+  }
 }
