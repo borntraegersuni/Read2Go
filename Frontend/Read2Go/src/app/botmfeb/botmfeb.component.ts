264ed52a24f882c080537c5d7ab1b08cc7d3b168
Maria Lazaridou <maria.lazaridou@zeiss.com>
2025-02-28 20:34:33 +0100

    modal for book club read more

--- Frontend/Read2Go/src/app/botmfeb/botmfeb.component.ts
+++ Frontend/Read2Go/src/app/botmfeb/botmfeb.component.ts
@@ -13,4 +13,11 @@ export class BotmfebComponent {
   bookImage: string = "ironflame.jpeg"; // Pfad zum Buchbild
   averageRating: number = 3; // Durchschnittliche Sternebewertung
   description: string = `Violet Sorrengail, who expected to die during her first year at Basgiath War College, faces brutal challenges and the threat of the new vice commandant, who aims to teach her just how powerless she is unless she betrays the man she loves, while simultaneously learning the crucial lesson that dragon riders make their own rules, and she realizes that her determination alone may not be enough to survive the hidden dangers the school holds.`;
+  isModalOpen: boolean = false; 
+  openModal() {
+    this.isModalOpen = true; // Modal öffnen
+  }
+  closeModal() {
+    this.isModalOpen = false; // Modal schließen
+  }
 }
